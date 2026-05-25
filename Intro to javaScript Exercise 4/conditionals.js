c9fa4959bdbae27e5c7efef2a921660fb99bfc8c
Adam Jattiem <adamjattiem12@gmail.com>
2026-05-25 12:08:43 +0200

    Add files via upload

---
+++ Intro to javaScript Exercise 4/conditionals.js
@@ -0,0 +1,16 @@
+//Adam: I finished assigning the variables and the if score. Need some help with the else statement. Can you help me out, Karah?
+
+let score = 75;
+const passingGrade = 60;
+
+//Karah: I got your back! Finished the if and else statement for you, but feel free to change the variable and test it out.
+
+if (score >= passingGrade) {
+  console.log("Passed");
+}
+else {
+  console.log("Failed");
+}
+if (score === 100) {
+  console.log("Perfect score! You aced the test!");
+}
